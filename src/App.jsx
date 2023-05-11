0a45a2a94e5299ae374c6b586fdbbddd49c848b2
stevim <i.m.steveim@gmail.com>
2023-05-11 15:46:14 -0400

    original main

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,55 @@
+import { Routes, Route } from 'react-router-dom'
+import ManliestThings from './pages/ManliestThings/ManliestThings'
+import StyledThings from './pages/StyledThings/StyledThings'
+import SillyThings from './pages/SillyThings/SillyThings'
+import Landing from './pages/Landing/Landing'
 
-    const davidsThings = [
+const App = () => {
+  const eugenesThings = [
     {
-      name: 'waffle',
+      name: 'video games',
       image:
-        'https://images.unsplash.com/photo-1568051243851-f9b136146e97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
-      attributes: ['tasty', 'fluffy', 'breakfast', 'yummy'],
+        'https://imgs.xkcd.com/comics/coffee.png',
+      attributes: [
+        'fun',
+        'anger inducing',
+        'expensive',
+        '$70 is too much for a video game',
+      ],
     },
     {
-      name: 'Liam',
-      image: 'https://i.imgur.com/l2qX34X.jpg',
-      attributes: ['fluffy', 'happy', 'sleepy', 'dumb'],
+      name: 'this is not a taco',
+      image:
+        'https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80',
+      attributes: [
+        'you are not looking at a taco',
+        'it is not a taco',
+        'your eyes are deceiving you',
+        'farewell',
+      ],
     },
     {
-      name: 'Poptart®',
-      image: 'https://i.imgur.com/q9zgcaP.jpg',
-      attributes: ['pastry?', 'food?', 'edible?', 'Horrible when toasted!'],
+      name: 'idk what this picture is but we will find out',
+      image: 'https://i.imgur.com/3BmfSOA.png',
+      attributes: ['what', 'is', 'this', 'Picture?'],
     },
   ]
 
+  const dawsonsThings = [
+    {
+      name: 'brown',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1200px-Flag_of_Turkey.svg.png',
+      attributes: ['fat'],
+    }
+  ]
+
+  const stephensThings = [
+    {
+      name: "canadians",
+      image: "https://imgs.xkcd.com/comics/canada.png",  
+      attributes: ["hockey","metric","loonie","its spelled colour"],
+    },
+  ]
 
   return (
     <Routes>
@@ -25,18 +57,20 @@
       <Route path="/" element={<Landing />} />
       <Route
         path="/the-manliest-things"
-        element={<ManliestThings things={bensThings} />}
+        element={<ManliestThings things={eugenesThings} />}
       />
       <Route
         path="/the-well-styled-things"
-        element={<StyledThings things={davidsThings} />}
+        element={<StyledThings things={dawsonsThings} />}
       />
       <Route
         path="/the-silly-things"
-        element={<SillyThings things={huntersThings} />}
+        element={<SillyThings things={stephensThings} />}
       />
     </Routes>
   )
 }
+
+console.log('ligma')
 
 export default App
