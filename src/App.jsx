b45a3bb0480bffd2246f98ffa6cf582f70fdbabb
Peter Lucido <petermlucido@gmail.com>
2023-05-11 14:17:10 -0500

    solve merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,55 @@ import SillyThings from './pages/SillyThings/SillyThings'
 import Landing from './pages/Landing/Landing'
 
 const App = () => {
+  const bensThings = [
+    {
+      name: 'banana',
+      image:
+        'https://images.unsplash.com/photo-1587132137056-bfbf0166836e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      attributes: [
+        'potassium-rich',
+        'yellow when good',
+        'not a taco',
+        'ring ring ring ring ring ring ring',
+      ],
+    },
+    {
+      name: 'taco',
+      image:
+        'https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80',
+      attributes: [
+        'yummy in my tummy',
+        'salsas may vary',
+        'definitely a taco',
+        'very edible',
+      ],
+    },
+    {
+      name: 'linux',
+      image: 'https://i.imgur.com/3BmfSOA.png',
+      attributes: ['not a taco', 'not Windows', 'not macOS', 'Penguins?'],
+    },
+  ]
+
+  const dawsonsThings = [
+    {
+      name: 'waffle',
+      image:
+        'https://images.unsplash.com/photo-1568051243851-f9b136146e97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+      attributes: ['tasty', 'fluffy', 'breakfast', 'yummy'],
+    },
+    {
+      name: 'Liam',
+      image: 'https://i.imgur.com/l2qX34X.jpg',
+      attributes: ['fluffy', 'happy', 'sleepy', 'dumb'],
+    },
+    {
+      name: 'Poptart®',
+      image: 'https://i.imgur.com/q9zgcaP.jpg',
+      attributes: ['pastry?', 'food?', 'edible?', 'Horrible when toasted!'],
+    },
+  ]
+
   const stephensThings = [
     {
       name: "canadians",
@@ -23,7 +72,7 @@ const App = () => {
       />
       <Route
         path="/the-well-styled-things"
-        element={<StyledThings things={davidsThings} />}
+        element={<StyledThings things={dawsonsThings} />}
       />
       <Route
         path="/the-silly-things"
